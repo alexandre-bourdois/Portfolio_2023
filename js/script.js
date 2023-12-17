f7afdef8f1d2ee5276385f84b36e9a1d876586be
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2023-12-17 11:46:56 +0100

    Experiences updated + added json

--- js/script.js
+++ js/script.js
@@ -19,3 +19,30 @@ VANTA.NET({
     points: 20.00,
     maxDistance: 19.00
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the element containing experiences
+    const experienceContainer = document.getElementById("experience-container");
+  
+    // Load JSON data
+    fetch("../Ressources/json/experiences.json")
+      .then(response => response.json())
+      .then(data => {
+        // Sort experiences by date (most recent first)
+        data.sort((a, b) => new Date(b.date) - new Date(a.date));
+  
+        // Generate HTML for each experience
+        data.forEach(experiencejson => {
+          const experienceHTML = `
+            <div class="experience_content">
+              <h3>${experiencejson.position} - ${experiencejson.company}</h3>
+              <p>${experiencejson.date} | ${experiencejson.location}</p>
+              <p>${experiencejson.description}</p>
+            </div>
+          `;
+          // Add experience to the page
+          experienceContainer.innerHTML += experienceHTML;
+        });
+      })
+      .catch(error => console.error("Error loading data:", error));
+  });
