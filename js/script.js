6d868467f836de017b65f44eda4409f921aa5927
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2023-12-23 21:50:32 +0100

    alternating experiences upgrade

--- js/script.js
+++ js/script.js
@@ -38,20 +38,27 @@ document.addEventListener("DOMContentLoaded", function () {
         // Determine alignment based on index
         const alignment = index % 2 === 0 ? "left" : "right";
 
-        const experienceHTML = `
-          <div class="experiences_content" style="text-align: ${alignment};">
-            <div class="position-text">
-              <h3>${experiencejson.position}</h3>
-              <h4>${experiencejson.company} | ${experiencejson.location}</h4>
-              <p>${experiencejson.date} </p>
-              <p>${experiencejson.description}</p>
-            </div>
-            <div class="position-image">
-              ${hasImage ? `<img src="${experiencejson.image}" alt="Company Logo" image" width="500px" height="400px">` : ""}
-              ${hasVideo ? `<video src="${experiencejson.video}" controls></video>` : ""}
-            </div>
+        const positionTextHTML = `
+        <div class="position-text">
+          <h3>${experiencejson.position}</h3>
+          <h4>${experiencejson.company} | ${experiencejson.location}</h4>
+          <p>${experiencejson.date} </p>
+          <div class="box-text">
+            <p>${experiencejson.description}</p>
           </div>
-        `;
+        </div>
+      `;
+        const positionImageHTML = `
+        <div class="position-image">
+          ${hasImage ? `<img src="${experiencejson.image}" alt="Company Logo" image" width="500px" height="400px">` : ""}
+          ${hasVideo ? `<video src="${experiencejson.video}" controls></video>` : ""}
+        </div>
+      `;      
+        const experienceHTML = `
+        <div class="experiences_content" style="text-align: ${alignment};">
+          ${alignment === "left" ? positionTextHTML + positionImageHTML : positionImageHTML + positionTextHTML}
+        </div>
+      `;
 
         experienceContainer.innerHTML += experienceHTML;
       });
