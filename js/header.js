0eaa41c08bcb4958557c315bffcc0f7562ab74ec
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-01-12 21:06:35 +0100

    navbar animation

---
+++ js/header.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const myImage = document.getElementById('navbar');
+
+    setTimeout(function() {
+        myImage.classList.add('animate');
+    }, 500);
+});
