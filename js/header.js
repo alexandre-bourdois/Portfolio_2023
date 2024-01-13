7376c778c5767a6a157440739c0784b1f183ff32
alexandre-bourdois <alexandrebourdois2001@gmail.com>
2024-01-13 16:36:01 +0100

    added translationX on portfolio title

--- js/header.js
+++ js/header.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     const myImage = document.getElementById('navbar');
+    const logo = document.getElementById('logo');
 
     setTimeout(function() {
         myImage.classList.add('animate');
+        logo.classList.add('animate');
     }, 100);
 });
